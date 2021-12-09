601df05ae2efefdd9a417d736f3de22cc84409a0
Alfred-Caron <58430242+Alfred-Caron@users.noreply.github.com>
2021-12-09 05:57:01 -0500

    Update DataParser.py

--- DataParser.py
+++ DataParser.py
@@ -1,55 +1,124 @@
-import pandas
+
 import re 
+import csv
+import fpdf
+from matplotlib.backends.backend_pdf import PdfPages
+from fpdf import FPDF
 
-##Imports that get added throughout the project will need to be added to specifications 
-
-#####file_Crawler()
-#goes through the data folder and makes an array for each grp 
-#this array can then be added to the comma_check to preformat the files
-
-
-######creates commas for files that need it and ignores the rest 
 def comma_Check(files):
-    for x in files:
-        filename = x
-        with open(filename, 'r') as f:
-            txt = f.read() #.replace(r"\s+", ",")
-        with open(filename, 'w') as f:
-            txt = re.sub(r'\s+"', ",\"", txt)
-            f.write(re.sub(r'"\s+', "\",", txt))
-comma_Check(array)
+        filename = []
+        with open(files, 'r') as f:
+                filename=f.read().split("\n")
+        filename.pop(0)
+        for element in filename:
+                with open("data/"+element, 'r') as f:
+                        txt = f.read() #.replace(r"\s+", ",")
+                with open("data/"+element, 'w') as f:
+                        txt = re.sub(r'\s+"', ",\"", txt)
+                        f.write(re.sub(r'"\s+', "\",", txt))
 
-########csv Reader GOING TO NEED SOME CHANGES BEFORE FINAL IMPLEMENTATION 
 def csv_Reader(files):
-    for x in files:
-        filename = x
+
+    filename = []
+    with open(files, 'r') as f:
+        filename=f.read().split("\n")
 # initializing the titles and rows list
-    fields = []
-    rows = []
+        
+        fields = []
+        rows = []
   
 # reading csv file
-    with open(filename, 'r') as csvfile:
+        filename.pop(0)
+        for element in filename:
+                with open("data/"+element, 'r') as csvfile:
     # creating a csv reader object
-         csvreader = csv.reader(csvfile)
+                        csvreader = csv.reader(csvfile)
+
       
     # extracting field names through first row
-        fields = next(csvreader)
-  
+                        fields = next(csvreader)
+                        rows.append(fields)
     # extracting each data row one by one
-        for row in csvreader:
-            rows.append(row)
-  
-    # get total number of rows
-        print("Total no. of rows: %d"%(csvreader.line_num))
-  
-# printing the field names
-    print('Field names are:' + ', '.join(field for field in fields))
-  
-#  printing first 5 rows
-    print('\nFirst 5 rows are:\n')
-    for row in rows[:1]:
-    # parsing each column of a row
-        for col in row:
-            print("%10s"%col),
-            print('\n')
-#add the return for a 3d array 
+                        for row in csvreader:
+                                rows.append(row)
+        return rows
+def write_pdf(fname, figures):
+    doc = PdfPages(fname)
+    for fig in figures:
+        fig.savefig(doc, format='pdf')
+    doc.close()
+
+def LtoN(array):
+        total_Count = []
+        x = 0
+        CONSTANT = 2
+        for row in array:
+                if (str(array[x][CONSTANT]) == "A"):
+                        total_Count.append(4)
+                elif(str(array[x][CONSTANT]) == "A-"):
+                        total_Count.append(3.7)
+                elif(str(array[x][CONSTANT]) == "B+"):
+                        total_Count.append(3.3)
+                elif(str(array[x][CONSTANT]) == "B"):
+                        total_Count.append(3)
+                elif(str(array[x][CONSTANT]) == "B-"):
+                        total_Count.append(2.7)
+                elif(str(array[x][CONSTANT]) == "C+"):
+                        total_Count.append(2.3)
+                elif(str(array[x][CONSTANT]) == "C"):
+                        total_Count.append(2)
+                elif(str(array[x][CONSTANT]) == "C-"):
+                        total_Count.append(1.7)
+                elif(str(array[x][CONSTANT]) == "D"):
+                        total_Count.append(1)
+                elif(str(array[x][CONSTANT]) == "F"):
+                        total_Count.append(0)
+                x += 1
+        perClass = []
+        x = 0
+        for row in array:
+                if (str(array[x][CONSTANT]) == "A"):
+                        perClass.append(4)
+                elif(str(array[x][CONSTANT]) == "A-"):
+                        perClass.append(3.7)
+                elif(str(array[x][CONSTANT]) == "B+"):
+                        perClass.append(3.3)
+                elif(str(array[x][CONSTANT]) == "B"):
+                        perClass.append(3)
+                elif(str(array[x][CONSTANT]) == "B-"):
+                        perClass.append(2.7)
+                elif(str(array[x][CONSTANT]) == "C+"):
+                        perClass.append(2.3)
+                elif(str(array[x][CONSTANT]) == "C"):
+                        perClass.append(2)
+                elif(str(array[x][CONSTANT]) == "C-"):
+                        perClass.append(1.7)
+                elif(str(array[x][CONSTANT]) == "D"):
+                        perClass.append(1)
+                elif(str(array[x][CONSTANT]) == "F"):
+                        perClass.append(0)
+                elif(str(array[x][CONSTANT]) == r''):
+                        perClass.append(str(array[x][0]))
+                x += 1 
+       
+        print('\n')
+      
+        return total_Count, perClass 
+def fromtext():
+        pdf = FPDF()   
+   
+        
+        pdf.add_page()
+        
+
+        pdf.set_font("Arial", size = 15)
+        
+     
+        f = open("readme.txt", "r")
+        
+       
+        for x in f:
+                pdf.cell(200, 10, txt = x, ln = 1, align = 'C')
+        
+       
+        pdf.output("results.pdf")  
